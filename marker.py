e678835e6975ad0612a97a29be6828badb9c4881
xzenon <forlanfr_8@hotmail.fr>
2022-10-28 16:43:00 +0100

    Handle exceptions

--- marker.py
+++ marker.py
@@ -5,6 +5,7 @@ import sys
 from typing import Any
 
 import requests
+from requests.exceptions import ConnectTimeout, Timeout
 
 def read_csv(file):
     with open(file, 'r') as f:
@@ -25,14 +26,14 @@ def is_valid(response: dict[str, Any]) -> bool:
         return False
     return True
 
-def write_to_csv(urls: list, type: str):
+def write_to_csv(urls: list, type: str) -> None:
     types = {"passed": "passed-interns.csv", "failed": "failed-interns.csv"}
     with open(types[type], 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["slackUsername"])
+        writer.writerow(["slackUsername", "endpoint"])
 
         for url in urls:
-            writer.writerow([url["username"]])
+            writer.writerow([url["username"], url["endpoint"]])
         file.close()    
 
 def marker(urls: list[dict[str, Any]]) -> None:
@@ -41,7 +42,7 @@ def marker(urls: list[dict[str, Any]]) -> None:
     for url in urls:
         try:
             # sending get request and saving the response as response object
-            response = requests.get(url=url["endpoint"])
+            response = requests.get(url=url["endpoint"], timeout=10)
             print(response.status_code)
 
             if response.status_code != 200:
@@ -51,10 +52,16 @@ def marker(urls: list[dict[str, Any]]) -> None:
                     failed.append(url)
                 else:
                     passed.append(url)    
-                    print(response.json())                
-        except Exception:
+                    print(response.json())     
+        except Timeout:
+            print(url)
             failed.append(url)
-            print("error")    
+            print('Request has timed out')                       
+        except Exception as exception:
+            print(url)
+            failed.append(url)
+            print ("OOps:",exception)
+
     write_to_csv(passed, "passed")
     write_to_csv(failed, "failed")
     print("passed", passed)
